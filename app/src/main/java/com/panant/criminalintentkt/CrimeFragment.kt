b858e05033fa06cdbae73b1d3ac17372dc01f64a
Anton <anton_panoff@msn.com>
2024-01-31 23:04:50 +0300

    Задействована кнопка даты с выводом календаря

--- app/src/main/java/com/panant/criminalintentkt/CrimeFragment.kt
+++ app/src/main/java/com/panant/criminalintentkt/CrimeFragment.kt
@@ -17,6 +17,7 @@ import java.util.UUID
 
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
+private const val DIALOG_DATE = "DialogDate"
 
 class CrimeFragment : Fragment() {
     private lateinit var crime: Crime
@@ -46,11 +47,6 @@ class CrimeFragment : Fragment() {
         titleField = view.findViewById(R.id.crime_title) as EditText
         dateButton = view.findViewById(R.id.crime_data) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
-
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
 
         return view
     }
@@ -88,6 +84,11 @@ class CrimeFragment : Fragment() {
         titleField.addTextChangedListener(titleWatcher)
         solvedCheckBox.apply {
             setOnCheckedChangeListener { _, isChecked -> crime.isSolved = isChecked }
+        }
+        dateButton.setOnClickListener{
+            DatePickerFragment().apply {
+                show(this@CrimeFragment.parentFragmentManager, DIALOG_DATE)
+            }
         }
     }
 
