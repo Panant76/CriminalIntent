5808947d9ba4c8a3c53d4f421cfbad256fa94d18
Anton <anton_panoff@msn.com>
2024-01-31 22:08:48 +0300

    Проба изменить RecyclerView.Adapter на ListAdapter

--- app/src/main/java/com/panant/criminalintentkt/CrimeListFragment.kt
+++ app/src/main/java/com/panant/criminalintentkt/CrimeListFragment.kt
@@ -8,11 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import java.util.UUID
 
@@ -25,7 +26,7 @@ class CrimeListFragment : Fragment() {
 
     private var callbacks: Callbacks? = null
     private lateinit var crimeRecyclerView: RecyclerView
-    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
+    //private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this)[CrimeListViewModel::class.java]
@@ -45,7 +46,7 @@ class CrimeListFragment : Fragment() {
 
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-        crimeRecyclerView.adapter = adapter
+        crimeRecyclerView.adapter = CrimeAdapter()
 
         return view
     }
@@ -69,8 +70,8 @@ class CrimeListFragment : Fragment() {
 
     private fun updateUI(crimes: List<Crime>) {
 
-        adapter = CrimeAdapter(crimes)
-        crimeRecyclerView.adapter = adapter
+        //adapter = CrimeAdapter(crimes)
+        (crimeRecyclerView.adapter as CrimeAdapter).submitList(crimes)
     }
 
     private inner class CrimeHolder(view: View) :
@@ -103,19 +104,31 @@ class CrimeListFragment : Fragment() {
         }
     }
 
-    private inner class CrimeAdapter(var crimes: List<Crime>) :
-        RecyclerView.Adapter<CrimeHolder>() {
+    private inner class CrimeAdapter : ListAdapter<Crime, CrimeHolder>(DiffCallback()) {
+
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
             val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
             return CrimeHolder(view)
         }
 
-        override fun getItemCount() = crimes.size
+        // override fun getItemCount() = getItem(po).size
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
-            val crime = crimes[position]
-            holder.bind(crime)
+            // val crime = crimes[position]
+            holder.bind(getItem(position))
         }
+    }
+
+    private inner class DiffCallback : ItemCallback<Crime>() {
+        override fun areItemsTheSame(oldItem: Crime, newItem: Crime): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Crime, newItem: Crime): Boolean {
+            return oldItem == newItem
+        }
+
     }
 
     companion object {
